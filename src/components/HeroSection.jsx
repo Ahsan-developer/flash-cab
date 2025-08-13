58b53d88f564f0e87676bc040a7158bdf87239f3
Ahsan-developer <ahsanriaz2466@gmail.com>
2025-08-13 11:33:54 +0500

    updated booking form

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -45,9 +45,72 @@ export default function HeroSection() {
         <div class="container">
           {/* <!-- Filter Form Start --> */}
           <form action="#" method="get">
-            <div class="row no-gutters align-items-center">
-              <div class="col-md-12">
+            <div class="row no-gutters align-items-center justify-content-center">
+              <div class="col-md-10 col-sm-9 col-12 ">
                 <div class="rent-details-box">
+                  <div class="rent-details-heading">
+                    <h2>book your ride instantly</h2>
+                  </div>
+                  <div class="rent-details-upper">
+                    <label htmlFor="">
+                      <input
+                        type="radio"
+                        name="from-airport"
+                        value="from-airport"
+                      />
+                      From airport
+                    </label>
+                    <label htmlFor="">
+                      <input
+                        type="radio"
+                        name="from-airport"
+                        value="from-airport"
+                      />
+                      To airport
+                    </label>
+                    <label htmlFor="">
+                      <input
+                        type="radio"
+                        name="from-airport"
+                        value="from-airport"
+                      />
+                      Point to Point
+                    </label>
+                  </div>
+                  <div class="rent-details-lower">
+                    <label htmlFor="">
+                      <i class="fa-solid fa-plane-up"></i>
+                      {/* <input
+                        type="text"
+                        placeholder="Pick-up location (airport, hotel name, railway station, street address...)"
+                      /> */}
+                      <select name="" id="">
+                        <option value="" disabled selected>
+                          Pick-up location
+                        </option>
+                        <option value="airport">Airport</option>
+                        <option value="hotel">Hotel</option>
+                        <option value="railway_station">Railway Station</option>
+                        <option value="street_address">Street Address</option>
+                      </select>
+                    </label>
+                    <label htmlFor="">
+                      <i class="fa-solid fa-location-dot"></i>
+                      {/* <input
+                        type="text"
+                        placeholder="Drop-off location (airport, hotel name, railway station, street address...)"
+                      /> */}
+                      <select name="" id="">
+                        <option value="" disabled selected>
+                          Pick-up location
+                        </option>
+                        <option value="airport">Airport</option>
+                        <option value="hotel">Hotel</option>
+                        <option value="railway_station">Railway Station</option>
+                        <option value="street_address">Street Address</option>
+                      </select>
+                    </label>
+                  </div>
                   <div class="rent-details-form">
                     {/* <!-- Rent Details Item Start --> */}
                     <div class="rent-details-item">
@@ -96,26 +159,6 @@ export default function HeroSection() {
                     {/* <!-- Rent Details Item Start --> */}
                     <div class="rent-details-item">
                       <div class="icon-box">
-                        <img src="/src/images/icon-rent-details-3.svg" alt="" />
-                      </div>
-                      <div class="rent-details-content">
-                        <h3>pickup date</h3>
-                        <p>
-                          <input
-                            type="text"
-                            name="date"
-                            placeholder="mm/dd/yyyy"
-                            class="rent-details-form datepicker"
-                            required
-                          />
-                        </p>
-                      </div>
-                    </div>
-                    {/* <!-- Rent Details Item End --> */}
-
-                    {/* <!-- Rent Details Item Start --> */}
-                    <div class="rent-details-item">
-                      <div class="icon-box">
                         <img src="/src/images/icon-rent-details-4.svg" alt="" />
                       </div>
                       <div class="rent-details-content">
@@ -135,18 +178,82 @@ export default function HeroSection() {
                     {/* <!-- Rent Details Item Start --> */}
                     <div class="rent-details-item">
                       <div class="icon-box">
+                        <img src="/src/images/icon-rent-details-3.svg" alt="" />
+                      </div>
+                      <div class="rent-details-content">
+                        <h3>pickup date</h3>
+                        <p>
+                          <input
+                            type="date"
+                            name="date"
+                            placeholder="mm/dd/yyyy"
+                            class="rent-details-form datepicker"
+                            required
+                          />
+                        </p>
+                      </div>
+                    </div>
+                    {/* <!-- Rent Details Item End --> */}
+
+                    {/* <!-- Rent Details Item Start --> */}
+                    <div class="rent-details-item">
+                      <div class="icon-box">
                         <img src="/src/images/icon-rent-details-5.svg" alt="" />
                       </div>
                       <div class="rent-details-content">
                         <h3>Return Date</h3>
                         <p>
                           <input
-                            type="text"
+                            type="date"
                             name="date"
                             placeholder="mm/dd/yyyy"
                             class="rent-details-form datepicker"
                             required
                           />
+                        </p>
+                      </div>
+                    </div>
+                    {/* <!-- Rent Details Item End --> */}
+
+                    {/* <!-- Rent Details Item Start --> */}
+                    <div class="rent-details-item">
+                      <div class="icon-box">
+                        <i class="fa-solid fa-person-walking-luggage"></i>
+                      </div>
+                      <div class="rent-details-content">
+                        <h3>Passengers</h3>
+                        <p>
+                          <select name="" id="">
+                            <option value="1">1</option>
+                            <option value="1">2</option>
+                            <option value="1">3</option>
+                            <option value="1">4</option>
+                            <option value="1">5</option>
+                            <option value="1">6</option>
+                            <option value="1">7</option>
+                          </select>
+                        </p>
+                      </div>
+                    </div>
+                    {/* <!-- Rent Details Item End --> */}
+
+                    {/* <!-- Rent Details Item Start --> */}
+                    <div class="rent-details-item">
+                      <div class="icon-box">
+                        <i class="fa-solid fa-bag-shopping"></i>
+                      </div>
+                      <div class="rent-details-content">
+                        <h3>Bags</h3>
+                        <p>
+                          <select name="" id="">
+                            <option value="1">1</option>
+                            <option value="1">2</option>
+                            <option value="1">3</option>
+                            <option value="1">4</option>
+                            <option value="1">5</option>
+                            <option value="1">6</option>
+                            <option value="1">7</option>
+                          </select>
                         </p>
                       </div>
                     </div>
