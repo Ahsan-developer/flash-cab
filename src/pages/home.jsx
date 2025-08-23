c7df42878a516eb4febcc3f15abfa1398ec16e18
Ahsan-developer <ahsanriaz2466@gmail.com>
2025-08-24 01:16:05 +0500

    fixed path issue

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Faqs from "../components/Faqs";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 import HowItWork from "../components/HowItWork";
 import WhyChooseUs from "../components/WhyChooseUs";
-import Footer from "../components/footer";
 import About from "./about";
 
 export default function Home() {
