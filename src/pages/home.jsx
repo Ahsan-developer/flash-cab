8347c7e977ed6688176888eaaf263683d33ea83f
Ahsan-developer <ahsanriaz2466@gmail.com>
2025-08-24 00:36:12 +0500

    set path in home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Faqs from "../components/Faqs";
 import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
 import HowItWork from "../components/HowItWork";
 import WhyChooseUs from "../components/WhyChooseUs";
 import Footer from "../components/footer";
-import HeroSection from "../components/heroSection";
 import About from "./about";
 
 export default function Home() {
